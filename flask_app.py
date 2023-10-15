986c889db2f5410eb8a0840f6ec03173373415d6
shu_crim <shucrim2020@gmail.com>
2023-10-15 23:30:43 +0900

    ユーザ管理機能・ユーザ登録機能を追加

--- flask_app.py
+++ flask_app.py
@@ -2,11 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for, make_respo
 from flask_httpauth import HTTPBasicAuth, HTTPDigestAuth
 from werkzeug.utils import secure_filename
 import json
+from werkzeug.security import generate_password_hash, check_password_hash
+import uuid
 
 import os
 import glob
 import datetime
 from enum import Enum
+import shutil
 
 
 INPUT_DATA_DIR = r"./input_data"
@@ -14,6 +17,23 @@ OUTPUT_DIR = r"./output"
 UPLOAD_DIR_ROOT = r"./upload_dir"
 ALLOWED_EXTENSIONS = set(['py'])
 TASK = {}
+USER_CSV_PATH = r"./data/users.csv"
+HASH_METHOD = "pbkdf2:sha256:260000"
+
+
+class UserData():
+    id : str
+    email : str
+    pass_hash : str
+    name : str
+    key : str
+
+    def __init__(self, id="", email="", pass_hash="", name="", key="") -> None:
+        self.id = id
+        self.email = email
+        self.pass_hash = pass_hash
+        self.name = name
+        self.key = key
 
 
 class Task():
@@ -41,6 +61,7 @@ class Stats():
 
 
 class Page(Enum):
+    HOME = 0,
     TASK = 1,
     BOARD = 2,
     LOG = 3,
@@ -48,7 +69,7 @@ class Page(Enum):
     ADMIN = 9
 
 
-#Flaskオブジェクトの生成
+# Flaskオブジェクトの生成
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024 #ファイルサイズ制限 2MB
 app.config['SECRET_KEY'] = 'secret key here'
@@ -56,6 +77,83 @@ auth = HTTPDigestAuth()
 auth_users = {
     "root": "password",
 }
+
+
+def ReadUsersCsv(path:str):
+    users = {}
+    try:
+        with open(path) as f:
+            f.readline() # ヘッダを読み飛ばす
+            while True:
+                line = f.readline()
+                if not line:
+                    break
+                email, id, name, key, pass_hash = line.rstrip().split(',')
+                users[id] = UserData(id, email, pass_hash, name, key)
+    except:
+        return None
+
+    return users
+
+
+def WriteUsersCsv(path:str, users:dict, must_backup:bool=True) -> bool:
+    # バックアップをとる
+    try:
+        shutil.copy2(path, os.path.join(os.path.dirname(path), "backup", datetime.datetime.now().strftime('users_%Y%m%d_%H%M%S_%f.csv')))
+    except:
+        if must_backup:
+            return False
+    
+    # 上書き作成する
+    try:
+        with open(path, mode='w', encoding='utf-8') as f:
+            f.write('email,id,name,key,pass-hash\n')
+            for id, user_data in users.items():
+                f.write(f"{user_data.email},{id},{user_data.name},{user_data.key},{user_data.pass_hash}\n")
+    except:
+        return False
+    
+    return True
+
+
+def AddUsersCsv(path:str, id:str, email:str, name:str, pass_hash:str, key:str) -> bool:
+    # ユーザデータを読み込む
+    users = ReadUsersCsv(path)
+    if users == None:
+        users = {}
+
+    # 重複チェック
+    if id in users:
+        return False
+
+    # ユーザデータを追加する
+    users[id] = UserData(id, email, pass_hash, name, key)
+
+    # 書き込んで結果を返す
+    return WriteUsersCsv(path, users, True if os.path.exists(path) else False)
+
+
+def UpdateUsersCsv(path:str, id:str, target:str, value:str) -> bool:
+    users = ReadUsersCsv(path)
+    if users == None:
+        return False
+    if not id in users:
+        return False
+    
+    # 更新する
+    if target == 'key':
+        users[id].key = value
+    elif target == 'name':
+        users[id].name = value
+    elif target == 'pass_hash':
+        users[id].pass_hash = value
+    elif target == 'email':
+        users[id].email = value
+    else:
+        return False
+
+    # 書き込んで結果を返す
+    return WriteUsersCsv(path, users)
 
 
 def GetUserStats(task_id) -> {}:
@@ -69,10 +167,16 @@ def GetUserStats(task_id) -> {}:
             return [0, 0, 0.0]
         return result
 
+    # ユーザ情報を読み込む
+    users = ReadUsersCsv(USER_CSV_PATH)
+
     file_paths = glob.glob(os.path.join(OUTPUT_DIR, task_id, "user", "*.csv"))
     stats = {}
     for file_path in file_paths:
-        user_name = os.path.splitext(os.path.basename(file_path))[0]
+        user_id = os.path.splitext(os.path.basename(file_path))[0]
+        if not user_id in users:
+            continue
+        user_name = users[user_id].name
         stats[user_name] = []
         
         with open(file_path, "r", encoding='shift_jis') as csv_file:
@@ -116,7 +220,7 @@ def GetUserStats(task_id) -> {}:
     return stats
 
 
-def menuHTML(page, task_id):
+def menuHTML(page, task_id=""):
     html = """
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div class="container-fluid">
@@ -126,6 +230,10 @@ def menuHTML(page, task_id):
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+    """
+
+    if page != Page.HOME:
+        html += """
                         <li class="nav-item">
                             <a class="nav-link{1} href="/{6}/task">{0}</a>
                         </li>
@@ -139,24 +247,31 @@ def menuHTML(page, task_id):
                             <a class="nav-link{4} href="/{6}/upload">提出</a>
                         </li>
                         {5}
+        """.format(
+            TASK[task_id].name,
+            " active\" aria-current=\"page\"" if page == Page.TASK else "\"",
+            " active\" aria-current=\"page\"" if page == Page.BOARD else "\"",
+            " active\" aria-current=\"page\"" if page == Page.LOG else "\"",
+            " active\" aria-current=\"page\"" if page == Page.UPLOAD else "\"",
+            """
+                        <li class="nav-item">
+                            <a class="nav-link active">管理者</a>
+                        </li>
+            """ if page == Page.ADMIN else "",
+            task_id
+        )
+
+    html += """
+                    </ul>
+                    <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a id="login-user-name" class="nav-link active" aria-current="page" href="/user/info"></a>
+                        </li>
                     </ul>
                 </div>
             </div>
         </nav>
-
-    """.format(
-        TASK[task_id].name,
-        " active\" aria-current=\"page\"" if page == Page.TASK else "\"",
-        " active\" aria-current=\"page\"" if page == Page.BOARD else "\"",
-        " active\" aria-current=\"page\"" if page == Page.LOG else "\"",
-        " active\" aria-current=\"page\"" if page == Page.UPLOAD else "\"",
-        """
-        <li class="nav-item">
-            <a class="nav-link active">管理者</a>
-        </li>
-        """ if page == Page.ADMIN else "",
-        task_id
-    )
+    """
 
     return Markup(html)
 
@@ -212,26 +327,50 @@ def CreateTable(stats_list, test=False, message=False, memo=False):
     return html_table, num_col
 
 
-def GetUserNames(task_id):
-    user_name_list = []
-    dir_list = glob.glob(os.path.join(UPLOAD_DIR_ROOT, task_id, "**/"))
-    for dir in dir_list:
-        # ディレクトリ名を取得→ユーザ名として使う
-        user_name = os.path.basename(os.path.dirname(dir))
-        user_name_list.append(user_name)
-
-    return user_name_list
-
-
 def CreateInProcHtml(task_id):
+    # ユーザ情報を読み込む
+    users = ReadUsersCsv(USER_CSV_PATH)
+
     inproc_text = ''
-    user_name_list = GetUserNames(task_id)
-    for user_name in user_name_list:
-        if os.path.exists(os.path.join(OUTPUT_DIR, task_id, "user", f"{user_name}_inproc")):
-            inproc_text += f"{user_name} さんの評価を実行中です。<br>"
+    for user_id in users:
+        if os.path.exists(os.path.join(OUTPUT_DIR, task_id, "user", f"{user_id}_inproc")):
+            inproc_text += f"{users[user_id].name} さんの評価を実行中です。<br>"
 
     return inproc_text + '<br>'
+
+
+def VerifyEmailAndPassword(email, password):
+    # ユーザ情報を読み込む
+    users = ReadUsersCsv(USER_CSV_PATH)
     
+    # 認証を行う
+    verified = False
+    user_data = UserData()
+    for id, user_data in users.items():
+        if user_data.email == email:
+            hash = HASH_METHOD + "$" + user_data.pass_hash.replace('-', '$')
+            if check_password_hash(hash, password):
+                verified = True
+            break
+    
+    return verified, user_data
+
+
+def VerifyIdAndKey(user_id, user_key):
+    # ユーザ情報を読み込む
+    users = ReadUsersCsv(USER_CSV_PATH)
+    
+    # 認証を行う
+    verified = False
+    if user_id in users:
+        user_data = users[user_id]
+        if user_data.key == user_key:
+            verified = True
+    else:
+        user_data = UserData()
+
+    return verified, user_data
+
 
 @auth.get_password
 def get_pw(username):
@@ -263,12 +402,117 @@ def index():
                 }
             )
     
-    return render_template(f'index.html', task_list_open=task_list_open, task_list_closed=task_list_closed)
+    return render_template(f'index.html', task_list_open=task_list_open, task_list_closed=task_list_closed, menu=menuHTML(Page.HOME))
 
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static/img'), 'favicon.ico', )
+
+
+@app.route('/join', methods=['GET', 'POST'])
+def join():
+    if request.method == 'GET':
+        return render_template(f'join.html', message="")
+    
+    elif request.method == 'POST':
+        try:
+            email = request.form['inputEmail']
+            password = request.form['inputPassword']
+            password_verify = request.form['inputPasswordVerify']
+            next_url = request.form['nextUrl']
+        except:
+            return render_template(f'join.html', message="入力データを受け取れませんでした。")
+        
+        # 2つのパスワード入力の一致チェック
+        if password != password_verify:
+            return render_template(f'join.html', message="再入力したパスワードが一致していません。")
+        
+        # ユーザ情報を読み込む
+        users = ReadUsersCsv(USER_CSV_PATH)
+
+        # email重複チェック
+        duplicate = False
+        for user_id, user_data in users.items():
+            if email == user_data.email:
+                duplicate = True
+                break
+        if duplicate:
+            return render_template(f'join.html', message="そのEmail addressは既に登録されています。")
+        
+        # ID発行
+        user_id = ""
+        for i in range(10000):
+            id = str(uuid.uuid4()).split('-')[0]
+            if id in users:
+                continue
+            else:
+                user_id = id
+                break
+
+        if user_id == "":
+            return render_template(f'join.html', message="IDを発行できませんでした。")
+        
+        # パスワードをハッシュ化
+        hash = generate_password_hash(password, salt_length=21)
+        pass_hash = hash.split('$')[1] + "-" + hash.split('$')[2]
+        
+        # 本人確認用のキーを作成
+        user_key = str(uuid.uuid4()).split('-')[0]
+
+        # ユーザ登録
+        name = email.split('@')[0]
+        success = AddUsersCsv(USER_CSV_PATH, user_id, email, name, pass_hash, user_key)
+
+        if not success:
+            return render_template(f'join.html', message="ユーザ情報を登録できませんでした。")
+
+        return render_template(f'user.html', user_email=email, user_id=user_id, user_key=user_key, user_name=name, next_url=next_url, login="true")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template(f'login.html', message="")
+    
+    elif request.method == 'POST':
+        try:
+            email = request.form['inputEmail']
+            password = request.form['inputPassword']
+            next_url = request.form['nextUrl']
+        except:
+            return render_template(f'login.html', message="入力データを受け取れませんでした。")
+        
+        # emailとパスワードで照合
+        verified, user_data = VerifyEmailAndPassword(email, password)
+        if not verified:
+            return render_template(f'login.html', message="Email addressかPasswordが誤っています。")
+
+        # 認証OKなので本人確認用のキーを作成して渡す
+        user_key = str(uuid.uuid4()).split('-')[0]
+
+        # キーを保存
+        UpdateUsersCsv(USER_CSV_PATH, user_data.id, 'key', user_key)
+
+        return render_template(f'user.html', user_email=email, user_id=user_data.id, user_key=user_key, user_name=user_data.name, next_url=next_url, login="true")
+
+
+@app.route('/user/info')
+def user():
+
+    return render_template(f'user.html', login="false")
+
+
+@app.route('/verify/<user_id>/<user_key>', methods=['GET'])
+def verify(user_id, user_key):
+    verified = False
+    
+    try:
+        verified = VerifyIdAndKey(user_id, user_key)
+    except:
+        verified = False
+
+    return "true" if verified else "false"
 
 
 @app.route('/<task_id>/')
@@ -343,45 +587,51 @@ def log(task_id):
     return render_template('log.html', task_name=TASK[task_id].name, table_log=Markup(html_table), menu=menuHTML(Page.LOG, task_id), inproc_text=Markup(inproc_text), num_col=num_col, task_id=task_id)
 
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
 @app.route('/<task_id>/upload', methods=['GET', 'POST'])
 def upload_file(task_id):
     if not task_id in TASK:
         return redirect(url_for('index'))
     
+    def allowed_file(filename):
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
     msg = ""
 
     if request.method == 'POST':
         file = request.files['file']
-        user = request.form['user']
         if not file:
             msg = "ファイルが選択されていません。"
         elif not allowed_file(file.filename):
             msg = "アップロードできるファイルは.pyのみです。"
         else:
-            try:
-                user = request.form['user']
-                save_dir = os.path.join(UPLOAD_DIR_ROOT, task_id, user)
-                if os.path.exists(save_dir):
-                    new_filename = secure_filename(file.filename)
-                    file.save(os.path.join(save_dir, new_filename))
-                    msg = f'{file.filename}がアップロードされました。'
-                    # メモを保存
-                    if request.form['memo'] != "":
-                        with open(os.path.join(save_dir, new_filename + '.txt'), mode='w', encoding='utf-8') as f:
-                            f.write(request.form['memo'])
-                else:
-                    raise(ValueError("アップロード先のディレクトリが存在しません。"))
-            except:
-                msg = "アップロードに失敗しました。"
+            user_id = request.form['user_id']
+            user_key = request.form['user_key']
+            
+            verified = VerifyIdAndKey(user_id, user_key)
+            if not verified:
+                msg = "ユーザ認証に失敗しました。"
+            else:
+                try:
+                    save_dir = os.path.join(UPLOAD_DIR_ROOT, task_id, user_id)
 
-    # ディレクトリ名からユーザ名のlistを作成
-    user_name_list = GetUserNames(task_id)
+                    # まだディレクトリが存在しなければ作成
+                    if not os.path.exists(save_dir):
+                        os.mkdir(save_dir)
 
-    return render_template('upload.html', task_id=task_id, task_name=TASK[task_id].name, message=msg, username=user_name_list, menu=menuHTML(Page.UPLOAD, task_id))
+                    if os.path.exists(save_dir):
+                        new_filename = secure_filename(file.filename)
+                        file.save(os.path.join(save_dir, new_filename))
+                        msg = f'{file.filename}がアップロードされました。'
+                        # メモを保存
+                        if request.form['memo'] != "":
+                            with open(os.path.join(save_dir, new_filename + '.txt'), mode='w', encoding='utf-8') as f:
+                                f.write(request.form['memo'])
+                    else:
+                        raise(ValueError("アップロード先のディレクトリが存在しません。"))
+                except:
+                    msg = "アップロードに失敗しました。"
+
+    return render_template('upload.html', task_id=task_id, task_name=TASK[task_id].name, message=msg, menu=menuHTML(Page.UPLOAD, task_id))
   
 
 @app.route('/<task_id>/admin')
