0f0631d13ea15d48c0ee8fbc9cbcd8f8975b82a1
shu_crim <shucrim2020@gmail.com>
2023-10-16 21:49:14 +0900

    ユーザの認証をして、ユーザ名を表示する

---
+++ static/js/user.js
@@ -0,0 +1,36 @@
+
+// ユーザ認証を行い、認証されたらメニュー右端の「ログイン」リンクの名前をユーザ名にする
+$(document).ready(function() {
+    var id_user_name = document.getElementById("login-user-name");
+
+    // ユーザ情報が保存されていたら取得
+    var user_id = localStorage.getItem("user_id");
+    var user_key = localStorage.getItem("user_key");
+    var user_name = localStorage.getItem("user_name");
+
+    // ユーザ情報がない
+    if (user_id == null || user_name == null || user_key == null){
+        id_user_name.textContent = "ログイン";
+        return;
+    }
+
+    // ユーザ情報を照合
+    var request = new XMLHttpRequest()
+    request.open("GET", "/verify/" + user_id + "/" + user_key, true);
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            //受信完了時の処理
+            var verify = document.createTextNode(decodeURI(request.responseText));
+
+            if (verify.data == "true") {
+                // 照合に成功したらユーザ名を表示
+                if (id_user_name != null) {
+                    id_user_name.textContent = user_name;
+                }
+            } else {
+                id_user_name.textContent = "ログイン";
+            }
+        }
+    }
+    request.send("");
+});
